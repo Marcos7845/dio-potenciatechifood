8a2639b5e2713efd17b1f3d11d0b321c5378cd44
Marcos Candido da Silva <142534113+Marcos7845@users.noreply.github.com>
2024-02-28 14:17:15 -0300

    teste
    teste

--- JS/ex001/media.js
+++ JS/ex001/media.js
@@ -1,18 +1,39 @@
 /* Algoritimo de média, classificar de acordo com a media se poassou ou nao, menor igual a 5 reprovado, maior que 5 e menor igual a 7 recuperação, maior que 7 aprovado*/
 
-const nota1 = 7;
-const nota2 = 7;
-const nota3 = 8;
+// const nota1 = 7;
+// const nota3 = 8;
 
-const media = (nota1+nota2+nota3)/3
+// const nota2 = 7;
+// const media = (nota1+nota2+nota3)/3
 
-if(media < 5){
-console.log('Você está Reprovado');
-} else if(media >= 5 && media<=7){
-console.log('Você está de recuperação');
-} else{
-console.log('Aprovado');
+// if(media < 5){
+// console.log('Você está Reprovado');
+// } else if(media >= 5 && media<=7){
+// console.log('Você está de recuperação');
+// } else{
+// console.log('Aprovado');
+// }
+
+// console.log(media)
+function calcularImposto(salario) {
+    let aliquota;
+    if (salario >= 0 && salario <= 1100) {
+        aliquota = 0.05;
+    }else if (salario>1100 && salario< 2500) {
+    aliquota=0.1
+    }else{
+    aliquota=0.15
+    }
+    //TODO Criar as demais condições para as aliquotas de 10.00% e 15.00%.
+    return aliquota * salario;
 }
 
-console.log(media)
+//Lê os valores de Entrada:
+const valorSalario = parseFloat(gets());
+const valorBeneficios = parseFloat(gets());
 
+//Calcula o imposto através da função "calcularImposto":
+const valorImposto = calcularImposto(valorSalario);
+//Calcula e imprime a Saída (com 2 casas decimais):
+const saida = valorSalario - valorImposto + valorBeneficios;
+print(saida.toFixed(2));
